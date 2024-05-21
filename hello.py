aadeaac2fe8518a321f926768c70eca83fd371bf
Vinicius Rodrigues <viniciuslourenco.r@gmail.com>
2024-05-21 14:14:40 -0300

    Add comentario e shebang

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
+#!C:\Users\vinic_wgutxj2\AppData\Local\Programs\Python\Python312-32\env python
+#!C:\Users\vinic_wgutxj2\AppData\Local\Programs\Python\Python312-32\python.exe
+#Comentário novo
+
 print("Hello,World!")
